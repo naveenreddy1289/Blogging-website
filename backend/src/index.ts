63fb0d0b6c97a2934901017a7c1c49c3afb9f7d1
Naveen Reddy <avnr1289@gmail.com>
2024-08-27 09:16:16 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,8 +16,4 @@ app.route('/api/v1/user',userRouter);
 app.route('/api/v1/blog',blogRouter);
 
 
-export default app  
-
-//postgresql://medium_owner:F8KhzU5vTGjb@ep-withered-snowflake-a4y1iyjs.us-east-1.aws.neon.tech/medium?sslmode=require
-
-// DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMmRiY2ViNzgtY2VjYi00MzA1LTk5MGMtNmJiMDc5Y2EzM2U5IiwidGVuYW50X2lkIjoiNTcwNDNjMmU2YmE1NGM0NGI2YzI4ZjBlNDVkN2QwZTQ3N2Y1YTFlNTYxZDk5ODNmNjI4YzQ0OTQ3MDJiYzU1NSIsImludGVybmFsX3NlY3JldCI6ImYwM2M2MmI2LWQ4MGEtNDU2MC04OTBiLWJlZmEwMmUyNjc5OSJ9.hTf9TwVqCv4Z27TNyGOh6Tdioyj0AJWUWN1OhyLWU4s"
+export default app
